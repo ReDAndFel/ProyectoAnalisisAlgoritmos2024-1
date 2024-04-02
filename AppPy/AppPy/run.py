d14caff4984c6da283c29dfab955522c0f51652b
ReDAndFel <pipecar366@gmail.com>
2024-04-02 17:37:26 -0500

    Se implemento el algoritmo NavOnArray y el algoritmo NaivLoopUnrollingTwo

--- AppPy/AppPy/run.py
+++ AppPy/AppPy/run.py
@@ -1,29 +1,50 @@
+import time
 import services.AlgorithmService as As
 import services.JsonService as Js
 
-json_times_file_path = "../../times.json"
+json_times_file_path = "../times.json"
 json_matrix_file_path = "../matrix.json"
 
 matrix1, matrix2 = Js.read_json_matrix(json_matrix_file_path)
 
-# Imprimir la matriz
+# Imprimir la matriz1
 print("La matriz1 desde el archivo JSON es:")
-for fila in matrix1:
-    for elemento in fila:
-        print(elemento, end=" ")  # Imprimir cada elemento de la fila seguido de un espacio
-    print()  # Imprimir un salto de línea al final de cada fila
+for row in matrix1:
+    for element in row:
+        print(element, end=" ")
+    print() 
     
-# Imprimir la matriz
+# Imprimir la matriz2
 print("La matriz2 desde el archivo JSON es:")
-for fila in matrix2:
-    for elemento in fila:
-        print(elemento, end=" ")  # Imprimir cada elemento de la fila seguido de un espacio
-    print()  # Imprimir un salto de línea al final de cada fila    
+for row in matrix2:
+    for element in row:
+        print(element, end=" ")
+    print()    
 
+# Se ejecuta el algoritmo NaivOnArray
+start_time = time.time()
+matrix_result_NaivOnArray = As.NaivOnArray(matrix1,matrix2)
+end_time = time.time()
+elapsed_time = end_time - start_time
+Js.modify_property(json_times_file_path,"NaivOnArray", elapsed_time)
+print("Tiempo de ejecución de NaivOnArray:", elapsed_time, "segundos")
+#Imprime la matriz resultante de NaivOnArray
+print("La matrix resultante del NaivOnArray es:")
+for row in matrix_result_NaivOnArray:
+    for element in row:
+        print(element, end=" ")
+    print()   
 
-# Modificar propiedad "NaivOnArray" en "Cs por el valor 1"
-#As.add_time(json_times_file_path,"NaivOnArray", 1)
-
-
-
-
+# Se ejecuta el algoritmo NaivLoopUnrollingTwo
+start_time = time.time()
+matrix_result_NaivLoopUnrollingTwo = As.NaivLoopUnrollingTwo(matrix1,matrix2)
+end_time = time.time()
+elapsed_time = end_time - start_time
+Js.modify_property(json_times_file_path,"NaivLoopUnrollingTwo", elapsed_time)
+print("Tiempo de ejecución de NaivLoopUnrollingTwo:", elapsed_time, "segundos")
+#Imprime la matriz resultante de NaivOnArray
+print("La matrix resultante del NaivLoopUnrollingTwo es:")
+for row in matrix_result_NaivOnArray:
+    for element in row:
+        print(element, end=" ")
+    print()   
--- AppPy/AppPy/services/AlgorithmService.py
+++ AppPy/AppPy/services/AlgorithmService.py
@@ -1,4 +1,41 @@
 from . import JsonService as js
 
-def add_time(json_file_path, algorithm_name, value):
-        js.modify_property(json_file_path, algorithm_name, value)
+def NaivOnArray(matrix_A, matrix_B):
+        
+    rows_A = len(matrix_A)
+    cols_A = len(matrix_A[0])
+    cols_B = len(matrix_B[0])
+
+    result = [[0.0] * cols_B for _ in range(rows_A)]  # Crear matriz de resultados
+
+    for i in range(rows_A):
+        for j in range(cols_B):
+            result[i][j] = 0.0  # Inicializar el elemento en (i, j) a 0.0
+            for k in range(cols_A):
+                result[i][j] += matrix_A[i][k] * matrix_B[k][j]
+
+    return result
+
+def NaivLoopUnrollingTwo(matrix_A, matrix_B):
+    N = len(matrix_A)
+    P = len(matrix_A[0])
+    M = len(matrix_B[0])
+    Result = [[0.0] * M for _ in range(N)]
+
+    if P % 2 == 0:
+        for i in range(N):
+            for j in range(M):
+                aux = 0.0
+                for k in range(0, P, 2):
+                    aux += matrix_A[i][k] * matrix_B[k][j] + matrix_A[i][k + 1] * matrix_B[k + 1][j]
+                Result[i][j] = aux
+    else:
+        PP = P - 1
+        for i in range(N):
+            for j in range(M):
+                aux = 0.0
+                for k in range(0, PP, 2):
+                    aux += matrix_A[i][k] * matrix_B[k][j] + matrix_A[i][k + 1] * matrix_B[k + 1][j]
+                Result[i][j] = aux + matrix_A[i][PP] * matrix_B[PP][j]
+
+    return Result
