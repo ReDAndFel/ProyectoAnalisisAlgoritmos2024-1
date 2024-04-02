30f628210829452ee42d3868615d47d0069fdb72
ReDAndFel <pipecar366@gmail.com>
2024-04-02 10:38:28 -0500

    Se cambio el apppy para leer las 2 matrices

--- AppPy/AppPy/run.py
+++ AppPy/AppPy/run.py
@@ -2,20 +2,27 @@ import services.AlgorithmService as As
 import services.JsonService as Js
 
 json_times_file_path = "../../times.json"
-json_matrix_file_path = "../../matrix.json"
+json_matrix_file_path = "../matrix.json"
 
-matrix = Js.read_json(json_matrix_file_path)
+matrix1, matrix2 = Js.read_json_matrix(json_matrix_file_path)
 
 # Imprimir la matriz
-print("La matriz desde el archivo JSON es:")
-for fila in matrix:
+print("La matriz1 desde el archivo JSON es:")
+for fila in matrix1:
     for elemento in fila:
         print(elemento, end=" ")  # Imprimir cada elemento de la fila seguido de un espacio
     print()  # Imprimir un salto de línea al final de cada fila
+    
+# Imprimir la matriz
+print("La matriz2 desde el archivo JSON es:")
+for fila in matrix2:
+    for elemento in fila:
+        print(elemento, end=" ")  # Imprimir cada elemento de la fila seguido de un espacio
+    print()  # Imprimir un salto de línea al final de cada fila    
 
 
 # Modificar propiedad "NaivOnArray" en "Cs por el valor 1"
-As.add_time(json_times_file_path,"NaivOnArray", 1)
+#As.add_time(json_times_file_path,"NaivOnArray", 1)
 
 
 
--- AppPy/AppPy/services/JsonService.py
+++ AppPy/AppPy/services/JsonService.py
@@ -15,4 +15,11 @@ def modify_property(json_file_path, property_name, value):
 def read_json(json_file_path):
     # Leer la matriz desde el archivo JSON
     with open(json_file_path, 'r') as json_matrix:
-        return json.load(json_matrix)              
+        return json.load(json_matrix) 
+          
+def read_json_matrix(json_matrix_file_path):
+    with open(json_matrix_file_path, 'r') as json_matrix:
+        data = json.load(json_matrix)
+        matrix1 = data.get("matrix1", [])  # Obtener la matriz1 del JSON, si no existe, retornar una lista vacía
+        matrix2 = data.get("matrix2", [])  # Obtener la matriz2 del JSON, si no existe, retornar una lista vacía
+        return matrix1, matrix2            
