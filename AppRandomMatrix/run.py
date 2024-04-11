bee5b8eb1827652e79b49e3fc70c112e2e0fc223
ReDAndFel <pipecar366@gmail.com>
2024-04-10 19:42:18 -0500

    Se cambio el apprandommatrix para que genere las matrices de los 8 casos desde 2 a la 4 a 2 a la 12

--- AppRandomMatrix/run.py
+++ AppRandomMatrix/run.py
@@ -15,17 +15,19 @@ def generar_arreglo_bidimensional(n):
         matrix.append(fila)
     return matrix
 
-def generar_dos_arreglos_bidimensionales():
-    n = 2 # exponencial
+def generar_dos_arreglos_bidimensionales(n):
     size = pow(2, n) #Crea una tamaño de 2 a la n
     print("Tamaño de la matriz:", size)  # Imprimir el valor del tamaño
     matrix1 = generar_arreglo_bidimensional(size)
     matrix2 = generar_arreglo_bidimensional(size)
     return matrix1, matrix2
 
-matrix1, matrix2 = generar_dos_arreglos_bidimensionales()
+data = {}
+for i in range(8):
+    n=3+(i+1)
+    case_name = f"caso{i+1}"
+    matrix1, matrix2 = generar_dos_arreglos_bidimensionales(n)
+    data[case_name] = {"matrix1": matrix1, "matrix2": matrix2}
 
-# Guardar los arreglos en un archivo JSON
-data = {"matrix1": matrix1, "matrix2": matrix2}
 with open(json_matrix_file_path, 'w') as file_json:
     json.dump(data, file_json)
