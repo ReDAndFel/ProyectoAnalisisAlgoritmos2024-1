7714061eb2e644c262a7a1b401baabd71c2ac5e0
Andres Felipe Castro <pipecar366@gmail.com>
2024-04-23 19:32:07 -0500

    Se captaron los tiempos de NaivLoopUnrollingTwo

--- AppPy/run.py
+++ AppPy/run.py
@@ -15,7 +15,7 @@ for i in range(8):
     print(f"Ejecucion caso {i+1}")
 
     print(f"Matriz de tamaño ", len(matrix1))
-
+    """
     # Se ejecuta el algoritmo NaivOnArray
     start_time = time.time()
     matrix_result_NaivOnArray = As.NaivOnArray(matrix1,matrix2)
@@ -31,7 +31,7 @@ for i in range(8):
     elapsed_time = end_time - start_time
     Js.modify_property(json_times_file_path,"NaivLoopUnrollingTwo", elapsed_time)
     print("Tiempo de ejecución de NaivLoopUnrollingTwo:", elapsed_time, "segundos")
-    
+    """
     # Se ejecuta el algoritmo NaivLoopUnrollingFour
     start_time = time.time()
     matrix_result_NaivLoopUnrollingFour = As.NaivLoopUnrollingFour(matrix1,matrix2)
