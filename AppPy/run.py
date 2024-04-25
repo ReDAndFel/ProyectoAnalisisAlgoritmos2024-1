bfb3375a63ea36844f16bdfa6807c7b907d2b3d3
Andres Felipe Castro <pipecar366@gmail.com>
2024-04-24 21:05:27 -0500

    Se captaron los tiempos de V.4 Parallel Block

--- AppPy/run.py
+++ AppPy/run.py
@@ -119,7 +119,7 @@ for i in range(8):
     elapsed_time = end_time - start_time
     Js.modify_property(json_times_file_path,"IV.5 Enhanced Parallel Block", elapsed_time)
     print("Tiempo de ejecución de IV.5 Enhanced Parallel Block:", elapsed_time, "segundos")
-    """   
+       
     # Se ejecuta el algoritmo V.3 Sequential block
     start_time = time.time()
     matrix_result_V3Sequentialblock = As.V3SequentialBlock(matrix1,matrix2)
@@ -134,4 +134,4 @@ for i in range(8):
     end_time = time.time()
     elapsed_time = end_time - start_time
     Js.modify_property(json_times_file_path,"V.4 Parallel Block", elapsed_time)
-    print("Tiempo de ejecución de V.4 Parallel Block:", elapsed_time, "segundos")"""
+    print("Tiempo de ejecución de V.4 Parallel Block:", elapsed_time, "segundos")
