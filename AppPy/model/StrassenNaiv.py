fe11c9319c7b3b2ddfe13bb3eb400370a6875fa3
Andres Felipe Castro <pipecar366@gmail.com>
2024-05-08 17:39:21 -0500

    Se corrigio el strassennaiv

--- AppPy/model/StrassenNaiv.py
+++ AppPy/model/StrassenNaiv.py
@@ -31,10 +31,10 @@ def strassen_naiv(matrizA, matrizB):
             
     # Asignamos en cada posición i,j de las nuevas matrices A y B los valores que están en las matrices A y B respectivamente
     for i in range(cantidadFilasMatrices):
-        for j in range(delimitadorMaximaIteracionesFilaColumna):
+        for j in range(cantidadColumnasMatrices):
             nuevaMatrizA[i][j] = matrizA[i][j]
             
-    for i in range(delimitadorMaximaIteracionesFilaColumna):
+    for i in range(cantidadFilasMatrices):
         for j in range(cantidadColumnasMatrices):
             nuevaMatrizB[i][j] = matrizB[i][j]
             
@@ -76,25 +76,23 @@ def strassen_naiv_step(matrizA, matrizB, matrizResultado, cantidadFilasMatrices,
         auxiliar4 = []
         auxiliar5 = []
         auxiliar6 = []
-        auxiliar7 = []
-
-        # Asignar memoria para cada fila
+        auxiliar7 = []        
         for i in range(nuevoTamanio):
             matrizA11.append([0] * nuevoTamanio)
             matrizA12.append([0] * nuevoTamanio)
             matrizA21.append([0] * nuevoTamanio)
             matrizA22.append([0] * nuevoTamanio)
-            
+                
             matrizB11.append([0] * nuevoTamanio)
             matrizB12.append([0] * nuevoTamanio)
             matrizB21.append([0] * nuevoTamanio)
             matrizB22.append([0] * nuevoTamanio)
-            
+                
             matrizResultadoParte11.append([0] * nuevoTamanio)
             matrizResultadoParte12.append([0] * nuevoTamanio)
             matrizResultadoParte21.append([0] * nuevoTamanio)
             matrizResultadoParte22.append([0] * nuevoTamanio)
-            
+                
             ayudante1.append([0] * nuevoTamanio)
             ayudante2.append([0] * nuevoTamanio)
             
@@ -120,44 +118,33 @@ def strassen_naiv_step(matrizA, matrizB, matrizResultado, cantidadFilasMatrices,
                 matrizB22[i][j] = matrizB[nuevoTamanio + i][nuevoTamanio + j]
         
         # computing the seven aux. variables
-        
-        ayudante1 = [list(map(sum, zip(filaA11, filaA22))) for filaA11, filaA22 in zip(matrizA11, matrizA22)]
-        ayudante2 = [list(map(sum, zip(filaB11, filaB22))) for filaB11, filaB22 in zip(matrizB11, matrizB22)]
-        
-        auxiliar1 = strassen_naiv_step(ayudante1, ayudante2, auxiliar1, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaA21, filaA22))) for filaA21, filaA22 in zip(matrizA21, matrizA22)]        
-        auxiliar2 = strassen_naiv_step(ayudante1, matrizB11, auxiliar2, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaA21, filaA22))) for filaA21, filaA22 in zip(matrizA21, matrizA22)]        
-        auxiliar3 = strassen_naiv_step(matrizA11, ayudante1, auxiliar3, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaB21, filaB11))) for filaB21, filaB11 in zip(matrizB21, matrizB11)]        
-        auxiliar4 = strassen_naiv_step(matrizA22, ayudante1, auxiliar4, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaA11, filaA12))) for filaA11, filaA12 in zip(matrizA11, matrizA12)]        
-        auxiliar5 = strassen_naiv_step(ayudante1, matrizB22, auxiliar5, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaA21, filaA11))) for filaA21, filaA11 in zip(matrizA21, matrizA11)]
-        ayudante2 = [list(map(sum, zip(filaB11, filaB12))) for filaB11, filaB12 in zip(matrizB11, matrizB12)]        
-        auxiliar6 = strassen_naiv_step(ayudante1, ayudante2, auxiliar6, nuevoTamanio, m)
-        
-        ayudante1 = [list(map(sum, zip(filaA12, filaA22))) for filaA12, filaA22 in zip(matrizA12, matrizA22)]
-        ayudante2 = [list(map(sum, zip(filaB21, filaB22))) for filaB21, filaB22 in zip(matrizB21, matrizB22)]        
-        auxiliar7 = strassen_naiv_step(ayudante1, ayudante2, auxiliar7, nuevoTamanio, m)
-        
-        # computing the four parts of the result
-        matrizResultadoParte11 = [list(map(sum, zip(filaAux1, filaAux4))) for filaAux1, filaAux4 in zip(auxiliar1, auxiliar4)]
-        matrizResultadoParte11 = [list(map(sum, zip(filaAux5, filaParte11))) for filaAux5, filaParte11 in zip(auxiliar5, matrizResultadoParte11)]
-        matrizResultadoParte11 = [list(map(sum, zip(filaAux7, filaParte11))) for filaAux7, filaParte11 in zip(auxiliar7, matrizResultadoParte11)]
-        
-        matrizResultadoParte12 = [list(map(sum, zip(filaAux3, filaAux5))) for filaAux3, filaAux5 in zip(auxiliar3, auxiliar5)]
-        
-        matrizResultadoParte21 = [list(map(sum, zip(filaAux3, filaAux5))) for filaAux3, filaAux5 in zip(auxiliar3, auxiliar5)]
-        
-        matrizResultadoParte22 = [list(map(sum, zip(filaAux1, filaAux3))) for filaAux1, filaAux3 in zip(auxiliar1, auxiliar3)]
-        matrizResultadoParte22 = [list(map(sum, zip(filaAux2, filaParte22))) for filaAux2, filaParte22 in zip(auxiliar2, matrizResultadoParte22)]
-        matrizResultadoParte22 = [list(map(sum, zip(filaAux6, filaParte22))) for filaAux6, filaParte22 in zip(auxiliar6, matrizResultadoParte22)]
+
+        plus(matrizA11, matrizA22, ayudante1, nuevoTamanio)
+        plus(matrizB11, matrizB22, ayudante2, nuevoTamanio)
+        strassen_naiv_step(ayudante1, ayudante2, auxiliar1, nuevoTamanio, m)
+        plus(matrizA21, matrizA22, ayudante1, nuevoTamanio)
+        strassen_naiv_step(ayudante1, matrizB11, auxiliar2, nuevoTamanio, m)
+        minus(matrizB12, matrizB22, ayudante1, nuevoTamanio)
+        strassen_naiv_step(matrizA11, ayudante1, auxiliar3, nuevoTamanio, m)
+        minus(matrizB21, matrizB11, ayudante1, nuevoTamanio)
+        strassen_naiv_step(matrizA22, ayudante1, auxiliar4, nuevoTamanio, m)
+        plus(matrizA11, matrizA12, ayudante1, nuevoTamanio)
+        strassen_naiv_step(ayudante1, matrizB22, auxiliar5, nuevoTamanio, m)
+        minus(matrizA21, matrizA11, ayudante1, nuevoTamanio)
+        plus(matrizB11, matrizB12, ayudante2, nuevoTamanio)
+        strassen_naiv_step(ayudante1, ayudante2, auxiliar6, nuevoTamanio, m)
+        minus(matrizA12, matrizA22, ayudante1, nuevoTamanio)
+        plus(matrizB21, matrizB22, ayudante2, nuevoTamanio)
+        strassen_naiv_step(ayudante1, ayudante2, auxiliar7, nuevoTamanio, m)
+        #computing the four parts of the result
+        plus(auxiliar1, auxiliar4, matrizResultadoParte11, nuevoTamanio)
+        minus(matrizResultadoParte11, auxiliar5, matrizResultadoParte11, nuevoTamanio)
+        plus(matrizResultadoParte11, auxiliar7, matrizResultadoParte11, nuevoTamanio)
+        plus(auxiliar3, auxiliar5, matrizResultadoParte12, nuevoTamanio)
+        plus(auxiliar2, auxiliar4, matrizResultadoParte21, nuevoTamanio)
+        plus(auxiliar1, auxiliar3, matrizResultadoParte22, nuevoTamanio)
+        minus(matrizResultadoParte22, auxiliar2, matrizResultadoParte22, nuevoTamanio)
+        plus(matrizResultadoParte22, auxiliar6, matrizResultadoParte22, nuevoTamanio)        
 
         # Almacenar resultados en la matriz resultado
         for i in range(nuevoTamanio):
@@ -181,4 +168,15 @@ def strassen_naiv_step(matrizA, matrizB, matrizResultado, cantidadFilasMatrices,
         matrizResultado = naiv_standard(matrizA, matrizB, matrizResultado, len(matrizA), len(matrizB), len(matrizResultado))
     
     return matrizResultado
+
+def plus(A, B, Result, newSize):
+    for i in range(newSize):
+        for j in range(newSize):
+            Result[i][j] = A[i][j] + B[i][j]
+
+def minus(A, B, Result, newSize):
+    for i in range(newSize):
+        for j in range(newSize):
+            Result[i][j] = A[i][j] - B[i][j]            
+
 
